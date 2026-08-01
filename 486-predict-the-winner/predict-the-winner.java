023f4edd9e09c5d78c2e394d1015197eee1abbd1
Mr CJ <145645743+CJ2k4@users.noreply.github.com>
2026-08-01 10:13:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (89.58%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -5,19 +5,23 @@ class Solution {
         for(int num : nums){
             total+= num;
         }
+        dp = new int[n][n];
+        for(int[] d : dp ){
+            Arrays.fill(d, -1);
+        }
         int p1Score= solve(nums, 0, n-1);
         if( p1Score >= total-p1Score)return true;
         return false;
     }
-
+    int[][] dp;
     int solve(int[] nums, int i, int j){
         if(i==j){
             return nums[i]; 
         }
         else if (i>j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
         int iScore = nums[i] + Math.min(solve(nums, i+2, j), solve(nums, i+1, j-1));
         int jScore = nums[j] + Math.min(solve(nums, i, j-2), solve(nums, i+1, j-1));
-        return Math.max(iScore, jScore);
-        
+        return dp[i][j]=Math.max(iScore, jScore);   
     }
 }

c6c1325dcc7bbc322d2c755f1dfc2f8b7491ef00
Mr CJ <145645743+CJ2k4@users.noreply.github.com>
2026-08-02 08:01:08 +0530

    Time: 9 ms (13.82%) | Memory: 47 MB (10.85%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        dp = new int[piles.length][piles.length];
+        for(int[] d : dp){
+            Arrays.fill(d, -1);
+        }
+        int alice = solve(piles, 0,piles.length-1);
+        int sum = Arrays.stream(piles).reduce(0,(a,b)->a+b);
+        return (alice > sum-alice);
+    }
+    int[][] dp;
+    int solve(int[] piles, int i, int j){
+        if(i==j)return piles[i];
+        else if(i>j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int iPiles = piles[i]+Math.min(solve(piles, i+2, j) ,solve(piles,i+1, j-1));
+        int jPiles = piles[j]+Math.min(solve(piles, i, j-2) ,solve(piles,i+1, j-1));
+        return dp[i][j]=Math.max(iPiles, jPiles);
+    }
+}

ba9259a0971e52e1c3b27adff1f5ec031197ac9b
Mr CJ <145645743+CJ2k4@users.noreply.github.com>
2026-09-02 07:57:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (99.02%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // boolean zero = false;
+        // boolean odd = false;
+        // boolean even = false;
+        // for(int i : nums1){
+        //     if(i==0){
+        //         zero=true;
+        //         continue;
+        //     }
+        //     if((i&1)!=0)odd=true;
+        //     else even = true;
+        // }
+        return true;
+    }
+}

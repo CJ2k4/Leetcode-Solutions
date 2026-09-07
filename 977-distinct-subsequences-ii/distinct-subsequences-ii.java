c153b01d114a1975cefea9bb8015f544d9bc245e
Mr CJ <145645743+CJ2k4@users.noreply.github.com>
2026-09-07 17:03:40 +0530

    Time: 4 ms (70.57%) | Memory: 43.7 MB (31.56%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.java
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -1,40 +1,37 @@
 class Solution {
     int MOD = 1000000007;
+
     public int distinctSubseqII(String s) {
         int n = s.length();
-
         dp = new long[n];
-        Arrays.fill(dp,-1);
-
+        Arrays.fill(dp, -1);
+        int[] lastSeen = new int[26];
         Arrays.fill(lastSeen, -1);
-
-        this.s =s;
-
+        this.s = s;
         prev = new int[n];
         Arrays.fill(prev, -1);
-        dp[0]=1;
-        for(int i =0; i<n; i++){
-            int c = s.charAt(i)-'a';
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
             prev[i] = lastSeen[c];
             lastSeen[c] = i;
         }
-
-        return (int)(solve(s.length()-1))%MOD;
+        return (int) (solve(s.length() - 1)) % MOD;
     }
 
     String s;
     long[] dp;
-    int[] lastSeen = new int[26];
     int[] prev;
-    
-    long solve(int n){
-        if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
 
-        long total = (2*solve(n-1)+1) %MOD;
-        int duplicates=0;
-        if(prev[n]!=-1) total = (total - (solve(prev[n]-1)+1) +MOD)%MOD;
-        
-        return dp[n]=(total);
+    long solve(int n) {
+        if (n < 0)
+            return 0;
+        if (dp[n] != -1)
+            return dp[n];
+        long total = (2 * solve(n - 1) + 1) % MOD;
+        int duplicates = 0;
+        if (prev[n] != -1)
+            total = (total - (solve(prev[n] - 1) + 1) + MOD) % MOD;
+        return dp[n] = (total);
     }
 }

90f64f99862fa3aef595c02bf8b47f4df40db0e7
Mr CJ <145645743+CJ2k4@users.noreply.github.com>
2026-09-07 12:20:39 +0530

    Time: 3 ms (99.79%) | Memory: 46.2 MB (90.30%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,70 +1,15 @@
 class Solution {
+    static int[] val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    static String[] sym = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     public String intToRoman(int num) {
-        int n = -1;
-        Deque<Integer> stack = new ArrayDeque<>();
-        while(num>0){
-            stack.push(num%10);
-            num/=10;
-            n++;
-        }
         StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()){
-            int curr = stack.poll();
-            if(n==3){
-                sb.append(String.valueOf('M').repeat(curr));
-            }else if(n==2){
-                if(curr != 4 && curr != 9){
-                    if(curr>=5){
-                        sb.append('D');
-                        curr=curr%5;
-                    };
-                    if(curr>0){
-                        sb.append(String.valueOf('C').repeat(curr));
-                    }
-                }
-                else{
-                    if(curr==4){
-                        sb.append("CD");
-                    }else{
-                        sb.append("CM");
-                    }
-                }
-            }else if(n==1){
-                if(curr != 4 && curr != 9){
-                    if(curr>=5){
-                        sb.append('L');
-                        curr=curr%5;
-                    };
-                    if(curr>0){
-                        sb.append(String.valueOf('X').repeat(curr));
-                    }
-                }
-                else{
-                    if(curr==4){
-                        sb.append("XL");
-                    }else{
-                        sb.append("XC");
-                    }
-                }
-            }else{
-                if(curr != 4 && curr != 9){
-                    if(curr>=5){
-                        sb.append('V');
-                        curr=curr%5;
-                    };
-                    if(curr>0){
-                        sb.append(String.valueOf('I').repeat(curr));
-                    }
-                }
-                else{
-                    if(curr==4){
-                        sb.append("IV");
-                    }else{
-                        sb.append("IX");
-                    }
-                }
+        for(int i = 0; i<13; i++){
+            if(num == 0)break;
+            int time = num/val[i];
+            while(time-->0){
+                sb.append(sym[i]);
             }
-            n--;
+            num = num%val[i];
         }
         return sb.toString();
     }

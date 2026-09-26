b7deb7ece3261838ff0d0cd7749135916be76bc3
Mr CJ <145645743+CJ2k4@users.noreply.github.com>
2026-09-26 18:57:26 +0530

    Time: 774 ms (5.43%) | Memory: 95.7 MB (37.56%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        Map<String, String> map = new HashMap<>();
+        for(List<String> ls: knowledge){
+            map.put(ls.get(0), ls.get(1));
+            System.out.println(ls.get(0));
+            System.out.println(ls.get(1));
+        }
+        int i = 0;
+        int n = s.length();
+        StringBuilder ans = new StringBuilder();
+        while(i<n){
+            if(isAlpha(s.charAt(i))){
+                ans.append(s.charAt(i));
+            }else if(s.charAt(i) == '('){
+                StringBuilder temp = new StringBuilder();
+                i++;
+                while(i<n && s.charAt(i)!= ')'){
+                    temp.append(s.charAt(i));
+                    i++;
+                }
+                System.out.print(temp);
+                if(map.containsKey(temp.toString())){
+                    ans.append(map.get(temp.toString()));
+                }
+                else ans.append('?');
+            }
+            i++;
+        }
+        return ans.toString();
+    }
+    boolean isAlpha(char ch){
+        return ch<='z' && ch>='a';
+    }
+}
